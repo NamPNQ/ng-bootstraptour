daee7f92f235e14fc268b083e8d6fe86572d8637
NamPNQ <nampnq@gmail.com>
2013-12-03 14:46:51 +0700

    Add factory $tour

--- assets/js/bootstrap-tour.js
+++ assets/js/bootstrap-tour.js
@@ -64,37 +64,54 @@ angular.module("ui.bootstrap.tour", [])
                 redirect: true,
                 orphan: false,
                 basePath: "",
-                template: "<div class='popover'>          <div class='arrow'></div>          <h3 class='popover-title'></h3>          <div class='popover-content'></div>          <div class='popover-navigation'>            <div class='btn-group'>              <button class='btn btn-sm btn-default' data-role='prev'>&laquo; Prev</button>              <button class='btn btn-sm btn-default' data-role='next'>Next &raquo;</button>            </div>            <button class='btn btn-sm btn-default' data-role='end'>End tour</button>          </div>        </div>",
-                afterSetState: function (key, value) {
-                },
-                afterGetState: function (key, value) {
-                },
-                afterRemoveState: function (key) {
-                },
-                onStart: function (tour) {
-                },
-                onEnd: function (tour) {
-                },
-                onShow: function (tour) {
-                },
-                onShown: function (tour) {
-                },
-                onHide: function (tour) {
-                },
-                onHidden: function (tour) {
-                },
-                onNext: function (tour) {
-                },
-                onPrev: function (tour) {
-                }
+                template: "<div class='popover'>          <div class='arrow'></div>          <h3 class='popover-title'></h3>          <div class='popover-content'></div>          <div class='popover-navigation'>            <div class='btn-group'>              <button class='btn btn-sm btn-default' data-role='prev'>&laquo; Prev</button>              <button class='btn btn-sm btn-default' data-role='next'>Next &raquo;</button>            </div>            <button class='btn btn-sm btn-default' data-role='end'>End tour</button>          </div>        </div>"
 
             },
             $get:['$injector', '$rootScope', '$q', '$http', '$templateCache', '$controller', '$tourStack',
                 function ($injector, $rootScope, $q, $http, $templateCache, $controller, $modalStack) {
                     var $tour = {};
                     $tour.Tour = function(tourOptions){
+                        var tourStartDeferred = $q.defer();
+                        var tourEndDeferred = $q.defer();
+                        var tourShowDeferred = $q.defer();
+                        var tourShownDeferred = $q.defer();
+                        var tourHideDeferred = $q.defer();
+                        var tourHiddenDeferred = $q.defer();
+                        var tourNextDeferred = $q.defer();
+                        var tourPrevDeferred = $q.defer();
+                        var tourInstance = {
+
+                        };
                         tourOptions = angular.extend({},$tourProvider.options,tourOptions);
                     };
+                    $tour.start = function(){
+
+                    };
+                    $tour.end = function(){
+
+                    };
+                    $tour.next = function(){
+
+                    };
+                    $tour.prev = function(){
+
+                    };
+                    $tour.goTo = function(){
+
+                    };
+                    $tour.restart = function(){
+
+                    };
+                    $tour.ended = function(){
+
+                    };
+                    $tour.addStep = function(){
+
+                    };
+                    $tour.addSteps = function(steps){
+
+                    };
+
                 }]
         };
         return $tourProvider
